f05eb2f0a95103b8588fc44f40ade5ff2918ef07
sani4forever <sani4forever@gmail.com>
2024-09-21 17:07:13 +0300

    Calendar button, select group, bindings, fixed nav_graph

--- app/src/main/java/com/example/accountinglectureattendance/fragments/AttendanceFragment.kt
+++ app/src/main/java/com/example/accountinglectureattendance/fragments/AttendanceFragment.kt
@@ -1,11 +1,15 @@
 package com.example.accountinglectureattendance.fragments
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.accountinglectureattendance.R
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.accountinglectureattendance.databinding.FragmentAttendanceBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -13,19 +17,47 @@ import com.example.accountinglectureattendance.R
  * create an instance of this fragment.
  */
 class AttendanceFragment : Fragment() {
+    private var _binding: FragmentAttendanceBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentAttendanceBinding.inflate(inflater, container, false)
+        val items = arrayOf("10701123", "10701223", "10701323")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, items)
+        binding.groupSpinner.adapter = adapter
+        binding.calendarButton.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            val datePickerDialog = DatePickerDialog(
+                requireContext(),
+                { _, selectedYear, selectedMonth, selectedDay ->
+                    val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                    Toast.makeText(requireContext(), "Вы выбрали дату: $selectedDate", Toast.LENGTH_SHORT).show()
+                },
+                year, month, day
+            )
+
+            datePickerDialog.show()
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_attendance, container, false)
+        return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 
     companion object {
         @JvmStatic
